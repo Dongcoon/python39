de40e33010aa1192f9d81e62b76f0d60c7b48b22
Dong <gongan1556@naver.com>
2022-10-11 18:18:38 +0900

    변수,연산자,예제

--- 01hello.py
+++ 01hello.py
@@ -1,2 +1,12 @@
-print('Hello, World!!')
-print('안녕 세상아')
+a = 'Hello World!'
+
+print(a*2)
+
+class A:
+    def __init__(self):
+        print("생성자")
+    def __del__(self):
+        print("소멸자")
+a = A()
+del a
+
---
+++ 04example.py
@@ -0,0 +1,66 @@
+# 1.
+print('*   *    **    ****    ****   *   *      /////')
+print('*   *   *  *   *   *   *   *  *   *     | o o |')
+print('*****  *    *  ****    ****    * *     (|  ^  |)')
+print('*   *  ******  *   *   *   *    *       | [_] |')
+print('*   *  *    *  *    *  *    *   *        -----')
+
+print('                     +---+')
+print('                     |   |')
+print('                 +---+---+')
+print('                 |   |   |')
+print('             +---+---+---+       /\\_/\\     ------')
+print('             |   |   |   |      ( \' \' )  / Hello \\')
+print('         +---+---+---+---+      (  -  ) <  Junior |')
+print('         |   |   |   |   |       | | |   \\ Coder!/')
+print('         +---+---+---+---+      (__|__)    ------')
+
+# 2.
+soju = ['소주', 3000,2]
+chic = ['너나치킨', 12000,1]
+money = 50000
+reg = '2014. 07. 07 14:35'
+
+print('[음식나라]')
+print('------------------')
+print(soju[0],'\t',2,'\t',soju[1]*soju[2])
+print(chic[0],'\t',1,'\t',chic[1]*chic[2])
+print('------------------')
+print('과세합계','\t\t',(soju[1]*soju[2]+chic[1]*chic[2])//10*9)
+print('부과세','\t\t',(soju[1]*soju[2]+chic[1]*chic[2])//10*1)
+print('------------------')
+print('총합계','\t\t',(soju[1]*soju[2]+chic[1]*chic[2]))
+print('받은금액','\t\t',money)
+print('잔돈','\t\t',money-(soju[1]*soju[2]+chic[1]*chic[2]))
+print('------------------')
+print(reg)
+
+# 3.
+# rate1, numberOfWindows
+
+# 4.
+
+# 6.
+x = 2.5; y = 1.5; m = 18; n = 4
+print(x+n*y-(x+n)*y)
+print(m / n + m % n)
+print(5 * x - n / 5)
+print(1 - (1 - (1 - (1 - (1 - n)))))
+
+# 7.
+print(3 + 4.5 * 2 + 27 / 8)
+print((True | False) & (3 < 4) | (5!=7))
+print(True | (3 < 5 & 6 >= 2))
+print(False == 'A')
+print((7 % 4 + 3 - 2 / 6) * 'Z')
+print()
+print()
+# 9.
+print(27 / 13 + 4)
+print()
+print()
+print()
+print()
+print()
+print()
+# 10.
